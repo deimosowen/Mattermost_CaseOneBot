3cd7d23c6c1633616a73c6df268d27c0489b1f0f
Denis Ushanev <deimosowen@gmail.com>
2024-03-02 15:56:04 +0300

    duty return date

--- commands/sendAs.js
+++ commands/sendAs.js
@@ -1,16 +1,21 @@
-const { postMessage } = require('../mattermost/utils');
+const { postMessage, postMessageInTreed, getChannelById } = require('../mattermost/utils');
 const { ADMIN_ID } = require('../config');
 const logger = require('../logger');
 
 module.exports = async ({ user_id, args }) => {
     try {
-        const [channel_id, message] = args;
+        const [id, message] = args;
 
         if (ADMIN_ID && (ADMIN_ID !== user_id)) {
             return;
         }
 
-        postMessage(channel_id, message);
+        const channel = await getChannelById(id);
+        if (channel) {
+            postMessage(id, message);
+        } else {
+            postMessageInTreed(id, message);
+        }
     } catch (error) {
         logger.error(`${error.message}\nStack trace:\n${error.stack}`);
     }
