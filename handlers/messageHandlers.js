728cb673a5af7080758e284b7524bd2fa9beb334
Denis Ushanev <deimosowen@gmail.com>
2024-06-29 10:05:23 +0300

    Jira: Checking task status

--- handlers/messageHandlers.js
+++ handlers/messageHandlers.js
@@ -1,9 +1,11 @@
 const handleQuestion = require('./handleQuestion');
 const handleMessageForwarding = require('./handleMessageForwarding');
 const handlePostDeleted = require('./handlePostDeleted');
+const handleTaskInReview = require('./handleTaskInReview');
 
 module.exports = {
     handleMessageForwarding,
     handleQuestion,
     handlePostDeleted,
+    handleTaskInReview,
 };
