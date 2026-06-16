0c4e3abc300ec8e7f523faea9dd030f9f73b0ef6
Denis Ushanev <deimosowen@gmail.com>
2026-06-16 20:12:53 +0300

    feat(config): add webhook and conflict resolver flags
    - Add AUTO_RESOLVE_CONFLICTS, GITLAB_WEBHOOK_SECRET, USE_GITLAB_WEBHOOK
    - gulpfile: add clean task, bump-patch/minor/major, exclude tests and lock files

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,59 @@
 const gulp = require('gulp');
 const bump = require('gulp-bump');
+const fs = require('fs');
+const path = require('path');
 
-gulp.task('bump-version', function () {
-    return gulp.src('./package.json')
-        .pipe(bump({ type: 'minor' }))
-        .pipe(gulp.dest('./'));
+// Очистка dist перед сборкой
+gulp.task('clean', function (done) {
+    const distDir = path.join(__dirname, 'dist');
+    if (fs.existsSync(distDir)) {
+        fs.rmSync(distDir, { recursive: true, force: true });
+    }
+    done();
 });
 
-gulp.task('build', function () {
+// Копирование файлов с правильными исключениями
+gulp.task('copy', function () {
     return gulp.src([
         '**',
         '!.git/**',
         '!node_modules/**',
         '!dist/**',
+        '!__tests__/**',
         '!tests/**',
         '!logs/**',
         '!.github/**',
-        '!db/**/*.db',
+        '!.cursor/**',
+        '!**/*.db',
         '!jira/proxy/node_modules/**',
         '!credentials**',
         '!gulpfile.js',
         '!.env',
-        '!**/.gitignore'
+        '!**/.gitignore',
+        '!package-lock.json',
+        '!**/package-lock.json',
     ], { dot: true })
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('release', gulp.series('bump-version', 'build'));
+gulp.task('build', gulp.series('clean', 'copy'));
+
+gulp.task('bump-patch', function () {
+    return gulp.src('./package.json')
+        .pipe(bump({ type: 'patch' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-minor', function () {
+    return gulp.src('./package.json')
+        .pipe(bump({ type: 'minor' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-major', function () {
+    return gulp.src('./package.json')
+        .pipe(bump({ type: 'major' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('release', gulp.series('bump-minor', 'build'));
